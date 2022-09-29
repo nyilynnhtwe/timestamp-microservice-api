45360da58307a210d495af5d35364c1f75edc095
nyilynnhtwe <nyilynnhtwe@gmail.com>
2022-09-29 21:32:54 +0630

    fixing

--- index.js
+++ index.js
@@ -1,35 +1,66 @@
+// index.js
+// where your node app starts
+
+// init project
 let express = require('express');
 let bodyParser = require('body-parser');
-const {
-    log
-} = require('console');
 let app = express();
 
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+let cors = require('cors');
+app.use(cors({
+    optionsSuccessStatus: 200
+})); // some legacy browsers choke on 204
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.get("/",(req,res)=>
-{
-    res.sendFile(__dirname+"/src/index.html");
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + '/src/index.html');
 });
 
-app.get("/api/:dateVar", (req, res) => {
-    let re = /^\d{4}-\d{2}-\d{1,2}\d*$/;
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+    res.json({
+        greeting: 'hello API'
+    });
+});
+
+app.get("/api/:dateVar?", (req, res) => {
     let parameter = req.params.dateVar;
-    let resJson,date;
-    if (parameter.match(re)) {
-        date = new Date(parameter);
+    let resJson, date;
+    if (!parameter) {
+        date = new Date();
     } else {
-        date = new Date(parseInt(parameter));
+        if (!isNaN(parameter)) {
+            date = new Date(parseInt(parameter));
+        } else {
+            date = new Date(parameter);
+        }
     }
-    let unixVar = date.getTime();
-    let gmtVar = date.toGMTString();
-    resJson = {
-        "unix": unixVar,
-        "utc" : gmtVar
+
+    if (date.toGMTString() === 'Invalid Date') {
+
+        res.json({
+            error: "Invalid Date"
+        });
+    } else {
+        let unixVar = date.getTime();
+        let gmtVar = date.toGMTString();
+        resJson = {
+            unix: unixVar,
+            utc: gmtVar
+        };
+        res.json(resJson);
     }
-    res.json(resJson);
 })
 
-app.listen(process.env.port || 3000);
+
+// listen for requests :)
+app.listen(process.env.PORT || 3000);
